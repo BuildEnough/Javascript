b7d625a4219d7bb527cedf2ade1f4644a6db0fb7
BuildEnough <rhtmaehcl878@naver.com>
2022-12-13 08:51:56 +0900

    docs: 자바스크립트 공부

--- 4_값의_할당.js
+++ 4_값의_할당.js
@@ -1,10 +1,13 @@
 // var score;
 // score = 80;
-
 // var score = 80;
 
-console.log(score)
+// console.log(score)
+// var score = 80;
+// console.log(score)
 
-var score = 80;
+// 값의 재할당
+// const foo = 10;
 
-console.log(score)
+// foo = 100;
+// console.log(foo)
